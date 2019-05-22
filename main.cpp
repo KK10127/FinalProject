ad397d070348d999995f9c93c433ab9569479c95
Jay Montoya <jayjoplin9898@gmail.com>
2019-05-21 23:54:46 -0700

    Initial commit - specifications declared

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*
+ * FINAL PROJECT
+ * CS 151
+ * Tu 5:30-9:20 with Porto
+ * Programmer: Jay Montoya
+ *
+ * PROBLEM STATEMENT:
+ *
+ * REQUIREMENTS:
+ *      3 Classes and a Driver
+ *      One DataStructure: ArrayList/Vector/LinkedList
+ *      ErrorChecking-ExceptionHandling
+ *      5-6 Pages of code
+ *      Binary File Handling
+ *      String Processing
+ *      Pointers
+ *      PolyMorphism/Virtual Functions
+ *
+ *
+ *
+ */
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
